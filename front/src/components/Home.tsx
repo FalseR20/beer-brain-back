264871f196774b2c2b9890e01492568b01062d7a
FalseR20 <kno3.2002@gmail.com>
2023-10-13 10:38:09 +0300

    refactor(front): update home bootstrap page

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,78 +1,63 @@
 import Page from "./Page.tsx";
-import "../css/Home.css"
-import {ReactNode, useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import { ReactNode, useEffect, useState } from "react";
 import getAuthHeader from "../authentication.ts";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
 interface DebtJSON {
-    id: number,
-    members_count: number,
-    date: string,
-    description: string,
-    is_closed: boolean
+  id: number,
+  members_count: number,
+  date: string,
+  description: string,
+  is_closed: boolean
 }
 
 export default function Home() {
-    return (
-        <Page isAuthRequired={true}>
-            <div id={"debts-div"}>
-                <div className={"debt-buttons"}>
-                    <Link className={"debt-button link-button"} to={"/create_event"}>Create event</Link>
-                    <Link className={"debt-button link-button"} to={"/join_event"}>Join event</Link>
-                </div>
-                <p>All debts</p>
-                <Debts/>
-            </div>
-
-        </Page>
-
-    )
+  return <Page isAuthRequired={true}>
+    <div className={"d-flex justify-content-between my-4"}>
+      <span className={"fs-1"}>All debts</span>
+      <div className={"d-flex flex-row-reverse"}>
+        <Button className={"fs-3 ms-4"} variant={"success"} size={"lg"} href={"/create_event"}>Create
+          event</Button>
+        <Button className={"fs-3"} variant={"outline-success  "} size={"lg"} href={"/join_event"}>Join event</Button>
+      </div>
+    </div>
+    <Debts />
+  </Page>;
 }
 
 function Debts(): ReactNode {
-    const [debts, setDebts] = useState<DebtJSON[]>([]);
+  const [debts, setDebts] = useState<DebtJSON[]>([]);
 
-    useEffect(
-        () => {
-            fetch("http://127.0.0.1:8000/core/common-events/", {
-                headers: getAuthHeader(),
-            }).then(response => response.json())
-                .then(data => setDebts(data));
-        }, []
-    )
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/core/common-events/", {
+      headers: getAuthHeader(),
+    }).then(response => response.json())
+      .then(data => setDebts(data));
+  }, []);
 
-    return (
-        <>
-            {debts.map(
-                (debt => Debt(debt))
-            )}
-        </>
+  return (<Row xs={1} md={2} className={"g-3"}>
+      {debts.map((debt => (<Col key={`Debt${debt.id}`}>
+        <Card className={"p-0"} border={debt.is_closed ? "secondary" : "primary"}>
+          <Card.Header>
+            <Row>
+              <Col>
+                <span>{debt.date}</span>
+              </Col>
+              <Col>
+                <span className={"text-end"}>{}</span>
+              </Col>
+            </Row>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>{debt.description}</Card.Title>
+            <Card.Text>{debt.members_count} members</Card.Text>
+            <Row className={"mx-0"}>
+              <Button variant={"primary"} size={"lg"} href={`/events/${debt.id}/`}>Look</Button>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>)))}
+    </Row>
 
-    )
-}
-
-
-function Debt(debtJSON: DebtJSON) {
-    console.log(debtJSON);
-    return (
-        <Link to={`/events/${debtJSON.id}/`} key={`Debt${debtJSON.id}`} className={"debt-div link-button"}>
-            <div className={"debt-top"}>
-                <div className={"debt-description"}>
-                    <p>{debtJSON.description}</p>
-                </div>
-                <div className={"debt-value"}>
-                    <p>{debtJSON.is_closed.toString()}</p>
-                </div>
-            </div>
-            <div className={"debt-bottom"}>
-                <div className={"debt-date"}>
-                    <p>{debtJSON.date}</p>
-                </div>
-                <div className={"debt-status"}>
-                    <p>{debtJSON.members_count} members</p>
-                </div>
-            </div>
-
-        </Link>
-    )
+  );
 }

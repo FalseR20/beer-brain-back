4b8e17413ba7b74bfd7956071f2353e278995076
FalseR20 <kno3.2002@gmail.com>
2023-10-15 19:13:19 +0300

    feat(front): update page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,25 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './components/Home.tsx';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home.tsx";
 import SignIn from "./components/SignIn.tsx";
 import SignUp from "./components/SignUp.tsx";
 import Event from "./components/Event.tsx";
 import NotFound from "./components/NotFound.tsx";
 import Guest from "./components/Guest.tsx";
+import "./css/App.scss";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/guest' element={<Guest/>}/>
-                <Route path='/sign_in' element={<SignIn/>}/>
-                <Route path='/sign_up' element={<SignUp/>}/>
-                <Route path='/events/:event_id' element={<Event/>}/>
-                <Route path='*' element={<NotFound/>}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/guest" element={<Guest />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/events/:event_id" element={<Event />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- front/src/components/Page.tsx
+++ front/src/components/Page.tsx
@@ -1,66 +1,87 @@
-import {ReactNode} from "react";
-import "../css/Page.scss"
-import {Link} from "react-router-dom";
-import {isAuthorized, signOut} from "../authentication.ts";
+import { ReactNode } from "react";
+import "../css/Page.scss";
+import { isAuthorized, signOut } from "../authentication.ts";
 import Guest from "./Guest.tsx";
-import {ThemeContext} from "../themeContext.tsx";
-import {BsMoon, BsSun} from "react-icons/bs";
-
+import { ThemeContext } from "../themeContext.tsx";
+import { BsMoon, BsSun } from "react-icons/bs";
+import { Button, Navbar } from "react-bootstrap";
 
 interface PageProps {
-    children?: ReactNode;
-    isAuthRequired?: boolean;
-}
-
-function out() {
-    signOut();
-    window.location.replace("/");
-}
-
-function Links() {
-    return (
-        <>
-            <ThemeContext.Consumer>
-                {({isDark, switchTheme}) => (
-                    <a className={"nav-sign"} onClick={switchTheme}>
-                        {isDark ? <BsSun/> : <BsMoon/>}
-                    </a>
-                )}
-            </ThemeContext.Consumer>
-
-            {
-                isAuthorized() ? (
-                    <>
-                        <a className={"nav-sign"} onClick={out}>Sign Out</a>
-                    </>
-                ) : (
-                    <>
-
-                        <Link className={"nav-sign"} to={"/sign_in"}>Sign In</Link>
-                        <Link className={"nav-sign"} to={"/sign_up"}>Sign Up</Link>
-                    </>
-                )
-            }
-        </>
-    )
-
+  children?: ReactNode;
+  isAuthRequired?: boolean;
 }
 
 export default function Page(props: PageProps) {
-    const inner: ReactNode = (props.isAuthRequired == true && !isAuthorized()) ? Guest() : props.children;
-    return (
-        <div>
-            <header>
-                <div>
-                    <Link id={"project-name"} className={"nav-sign"} to={"/"}>BeerBrain</Link>
-                    <nav>
-                        {Links()}
-                    </nav>
-                </div>
-            </header>
-            <div id={"common-field"}>
-                {inner}
-            </div>
-        </div>
-    )
+  const inner: ReactNode =
+    props.isAuthRequired == true && !isAuthorized() ? Guest() : props.children;
+  return (
+    <>
+      <header className="d-flex flex-row justify-content-center bg-body-tertiary">
+        <Navbar
+          className={
+            "d-flex flex-row justify-content-between align-items-center width-60"
+          }
+        >
+          <Navbar.Brand href={"/"} className={"fs-3"}>
+            BeerBrain
+          </Navbar.Brand>
+          <Navbar.Collapse className={"justify-content-end"}>
+            <SignGroup />
+            <ThemeSwitcher />
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+      <div id={"common-field"} className={"mx-auto width-60"}>
+        {inner}
+      </div>
+    </>
+  );
+}
+
+function ThemeSwitcher() {
+  return (
+    <>
+      <ThemeContext.Consumer>
+        {({ isDark, switchTheme }) => (
+          <a
+            id={"theme-switcher"}
+            className={"fs-2 text-body pb-2"}
+            onClick={switchTheme}
+          >
+            {isDark ? <BsSun /> : <BsMoon />}
+          </a>
+        )}
+      </ThemeContext.Consumer>
+    </>
+  );
+}
+
+function SignGroup() {
+  return isAuthorized() ? (
+    <>
+      <Button
+        variant={"outline-secondary"}
+        className={"me-3"}
+        onClick={() => {
+          signOut();
+          window.location.replace("/");
+        }}
+      >
+        Sign Out
+      </Button>
+    </>
+  ) : (
+    <>
+      <Button
+        variant={"outline-secondary"}
+        className={"me-3"}
+        href={"/sign_in"}
+      >
+        Sign In
+      </Button>
+      <Button variant={"secondary"} className={"me-3"} href={"/sign_up"}>
+        Sign Up
+      </Button>
+    </>
+  );
 }

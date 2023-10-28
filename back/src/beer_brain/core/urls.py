7ecd421871f38f3a3be6d8439f58c15310c6638d
FalseR20 <kno3.2002@gmail.com>
2023-10-28 18:01:03 +0300

    fix(back): fix urls

--- back/src/beer_brain/core/urls.py
+++ back/src/beer_brain/core/urls.py
@@ -4,10 +4,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path("auth/user/new", views.UserCreateAPIView.as_view()),
+    path("auth/user/new/", views.UserCreateAPIView.as_view()),
     path("auth/get-token/", obtain_auth_token, name="api_token_auth"),
-    path("core/events/all", views.EventListAPIView.as_view()),
-    path("core/events/new", views.EventCreateAPIView.as_view()),
+    path("core/events/all/", views.EventListAPIView.as_view()),
+    path("core/events/new/", views.EventCreateAPIView.as_view()),
     path("core/events/<int:event_id>/join/", views.MemberAPIView.as_view()),
     path("core/events/<int:pk>/", views.EventRetrieveUpdateAPIView.as_view()),
     path("core/full-events/<int:pk>/", views.FullEventRetrieveUpdateAPIView.as_view()),

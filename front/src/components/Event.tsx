8bf2b4ace43e6a4ad40aee25ec5f74c5ce711b17
FalseR20 <kno3.2002@gmail.com>
2023-11-26 20:28:29 +0300

    feat(front): update event page header

--- front/src/components/Event.tsx
+++ front/src/components/Event.tsx
@@ -5,7 +5,7 @@ import NotFound from "./NotFound.tsx";
 import getAuthHeader from "../authentication.ts";
 import "../css/Event.css";
 import { Button, Form, ListGroup, Modal } from "react-bootstrap";
-import { BsPencil } from "react-icons/bs";
+import { BsThreeDotsVertical } from "react-icons/bs";
 import { URLS } from "../constants.ts";
 import { RxPlus } from "react-icons/rx";
 import { Avatar48 } from "./Profile.tsx";
@@ -57,126 +57,149 @@ export default function Event() {
 }
 
 function EventValidated({ event }: { event: IEvent }) {
+  return (
+    <Template doAddWrapping={false}>
+      <div id={"common-field"} className={"width-60"}>
+        <EventHeader event={event} />
+        {/*<Members event={event} />*/}
+        {/*<Payers event={event} />*/}
+        {/*<SettingsElements />*/}
+      </div>
+    </Template>
+  );
+}
+
+function EventHeader({ event }: { event: IEvent }) {
   const [showDescriptionModal, setShowDescriptionModal] = useState(false);
   const [description, setDescription] = useState(event.description);
   const [newDescription, setNewDescription] = useState<string | null>(null);
 
-  const membersWithDposits = event.members.filter(
-    (member) => member.deposits.length > 0,
-  );
-
   return (
-    <Template>
-      <div
-        className={
-          "border rounded-bottom-4 bg-body-tertiary p-2 d-flex flex-row"
-        }
-      >
+    <div className={"border border-top-0 rounded-bottom-5 bg-body-tertiary"}>
+      <div className={"d-flex flex-row ps-2 pe-3 py-1"}>
         <div
-          className={"border rounded-bottom-4"}
+          className={"border rounded-5 my-1 me-3"}
           style={{
-            height: "3rem",
-            width: "3rem",
+            height: "3.5rem",
+            width: "3.5rem",
             fontSize: "1.5rem",
             padding: "0.5rem",
             textAlign: "center",
           }}
         >
-          {event.id}
+          {parseInt(event.date.slice(8, 10))}
         </div>
-        <div className={"d-flex align-items-center border-bottom mb-2"}>
-          <h1 className={"mb-2 flex-grow-1"}>{description}</h1>
-          <Button
-            variant={"outline-secondary"}
-            size={"lg"}
+        <div className={"flex-grow-1"}>
+          <div className={"d-flex flex-column align-items-start"}>
+            <div className={"fs-4"}>{description}</div>
+            <div
+              className={"fs-6"}
+            >{`${event.date}, ${event.members.length} members`}</div>
+          </div>
+        </div>
+
+        <div className={"pt-1 ms-2"}>
+          <a
             id={"name-addon"}
-            className={"p-1 border rounded-5"}
+            className={"text-body fs-2 hover-cursor-pointer"}
             onClick={() => setShowDescriptionModal(true)}
           >
-            <div className={"pb-1 px-2"}>
-              <BsPencil />
-            </div>
-          </Button>
+            <BsThreeDotsVertical />
+          </a>
+          <Modal
+            show={showDescriptionModal}
+            onHide={() => setShowDescriptionModal(false)}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Description updating</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>New description</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    defaultValue={description}
+                    onChange={(e) => {
+                      setNewDescription(e.target.value);
+                    }}
+                  />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                variant="outline-secondary"
+                onClick={() => setShowDescriptionModal(false)}
+              >
+                Close
+              </Button>
+              <Button
+                id={"btn-modal-ok"}
+                variant="primary"
+                onClick={() => {
+                  if (newDescription != description) {
+                    console.log(`New description ${newDescription}`);
+                    setDescription(newDescription!);
+                  }
+                  setShowDescriptionModal(false);
+                }}
+              >
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-        <Modal
-          show={showDescriptionModal}
-          onHide={() => setShowDescriptionModal(false)}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Description updating</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>New description</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  defaultValue={description}
-                  onChange={(e) => {
-                    setNewDescription(e.target.value);
-                  }}
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button
-              variant="outline-secondary"
-              onClick={() => setShowDescriptionModal(false)}
-            >
-              Close
-            </Button>
-            <Button
-              id={"btn-modal-ok"}
-              variant="primary"
-              onClick={() => {
-                if (newDescription != description) {
-                  console.log(`New description ${newDescription}`);
-                  setDescription(newDescription!);
-                }
-                setShowDescriptionModal(false);
-              }}
-            >
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
-      <div className={"mb-1"}>
-        <h5>
-          {event.date}, {event.members.length} people
-        </h5>
-        <ListGroup horizontal={true}>
-          {event.members.map((member) => {
-            // noinspection RequiredAttributes
-            return (
-              <div
-                className={"m-2"} //  border-bottom border-light rounded-2
-                key={member.id}
-              >
-                <Link to={`/user/${member.id}`}>
-                  <Avatar48 id={member.id} />
-                </Link>
-              </div>
-            );
-          })}
-          <div className={"m-2"}>
-            <RxPlus size={48} />
-          </div>
-        </ListGroup>
-      </div>
+    </div>
+  );
+}
 
+function Members({ event }: { event: IEvent }) {
+  return (
+    <div className={"mb-1"}>
+      <h5>
+        {event.date}, {event.members.length} people
+      </h5>
+      <ListGroup horizontal={true}>
+        {event.members.map((member) => {
+          // noinspection RequiredAttributes
+          return (
+            <div
+              className={"m-2"} //  border-bottom border-light rounded-2
+              key={member.id}
+            >
+              <Link to={`/user/${member.id}`}>
+                <Avatar48 id={member.id} />
+              </Link>
+            </div>
+          );
+        })}
+        <div className={"m-2"}>
+          <RxPlus size={48} />
+        </div>
+      </ListGroup>
+    </div>
+  );
+}
+
+function Payers({ event }: { event: IEvent }) {
+  const membersWithDeposits = event.members.filter(
+    (member) => member.deposits.length > 0,
+  );
+  return (
+    <>
       <div className={"d-flex justify-content-between align-items-center"}>
         <h2 className={"my-3"}>Payers</h2>
         <Button variant={"success"}>Add payment</Button>
       </div>
       <div className={"border border-secondary rounded-3 p-2"}>
         <ListGroup className={""} variant={"flush"}>
-          {membersWithDposits.length > 0 ? (
+          {membersWithDeposits.length > 0 ? (
             event.members.map((member) =>
               member.deposits.map((deposit) => (
                 <ListGroup.Item key={deposit.id}>
@@ -193,8 +216,7 @@ function EventValidated({ event }: { event: IEvent }) {
           )}
         </ListGroup>
       </div>
-      <SettingsElements />
-    </Template>
+    </>
   );
 }
 

beffbc97ee4ccbf2b58565f96cbcf7bd1c7ce43e
FalseR20 <kno3.2002@gmail.com>
2024-03-11 13:14:33 +0300

    feat: deny access to event if you are not a member

--- beer_brain/events/permissions.py
+++ beer_brain/events/permissions.py
@@ -8,7 +8,7 @@ class EventEditOnlyHost(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj: models.Event):
         if request.method in permissions.SAFE_METHODS:
-            return True
+            return obj.users.filter(id=request.user.id).exists()
         return obj.host == request.user
 
 
